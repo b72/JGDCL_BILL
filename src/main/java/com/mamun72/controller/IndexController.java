e81e7a50d8cf6a03e8d8968a1110b8797b8d3196
Mamun <abdullah-mamun.it@nblbd.com>
2020-02-05 15:52:49 +0600

    error page added

--- src/main/java/com/mamun72/controller/IndexController.java
+++ src/main/java/com/mamun72/controller/IndexController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -41,5 +43,16 @@ public class IndexController {
     @RequestMapping(value = "/saveBill", method = RequestMethod.POST)
     public String saveBill() {
         return "index";
+    }
+
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public @ResponseBody String login(
+            @RequestParam("userName") String userName,
+            @RequestParam("BranchCodeint") String BranchCodeint,
+            @RequestParam("brName") String brName,
+                                      Model model){
+
+        //model.addAttribute("test", token);
+        return userName + BranchCodeint + brName;
     }
 }
