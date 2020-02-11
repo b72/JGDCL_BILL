61ed21bfe5e4c0e697534ad75f175aa646c897c4
maksudul.it <maksudul.it@nationalbankbd.com>
2020-02-11 19:09:21 +0600

    ajax loader gif added

--- src/main/resources/static/js/ajax_call.js
+++ src/main/resources/static/js/ajax_call.js
@@ -1,36 +1,48 @@
 $(document).ready(function () {
     $("#get-bill").submit(function (e) {
-        alert('on submit');
-        var table = document.getElementById("displayTable")
         e.preventDefault();
         var id = $('#get-bill input[name="customer_id"]').val();
         if (id) {
             $.ajax({
                 method: "GET",
-                url: "/ajax/getCustomerById?customerId="+id,
+                url: "/ajax/getCustomerById?customerId=" + id,
                 headers: {"X-CSRF-TOKEN": $('#get-bill input[name="_csrf"]').val()},
+                beforeSend: function(){
+                    $(".loader").show();
+                    $(".ajax-result").html("");
+                },
                 success: function (response) {
-                    $('#result').html(response);
+                    $(".loader").hide();
                     console.log(response);
+                    console.log(typeof response);
 
-                    alert("The account" + response.toString());
-                    var game_name = [];
-                    for (var key in response) {
-                        if (response.hasOwnProperty(key)) {
-                            var item = response[key];
-                            game_name.push({
-                                ItemName: item.data.name //Changing the .name to .dlc or .type will then display that result
-
-                            });
-                        }
-                    }
-                    for (var i = 0; i < game_name.length; i++) {
-                        table.append("<tr><td>" + game_name[i].ItemName + "</td></tr>");
+                    if (typeof response === "object") {
+                        var html = ""
+                        $.each(response, function f(key, value) {
+                            var key1 = key.charAt(0).toUpperCase() + key.slice(1);
+                            var key2 = key1.replace(/([A-Z])/g, ' $1').trim();
+                            html += '<tr><th> ' + key2 + ' </th><td> ' + value + ' </td></tr>';
+                        });
+                        console.log(html);
+                        $(".ajax-result").html(html);
                     }
                 },
                 error: function (e) {
+                    $(".loader").hide();
+                    var html = "";
+                    $.each(e, function f(key, value) {
+                        var key1 = key.charAt(0).toUpperCase() + key.slice(1);
+                        var key2 = key1.replace(/([A-Z])/g, ' $1').trim();
+                        if(key.toString() === 'message'){
+                            html += '<tr><th> ' + key2 + ' </th><td> ' + value + ' </td></tr>';
+                            alert(e);
+                        }
+                        // html += '<tr><th> ' + key2 + ' </th><td> ' + value + ' </td></tr>';
+                    });
                     console.log(e);
-                    alert(response.responseText);
+                    $(".ajax-result").html(html);
+
+
 
                 }
             })
